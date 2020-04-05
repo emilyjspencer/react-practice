37e9bece3fd9920da0261acfe998cf465d86654f
Emily Spencer <emilyspencer89@hotmail.co.uk>
2020-04-05 17:38:45 +0100

    Renders each item in an array using JSX and mapping through the array

--- src/index.js
+++ src/index.js
@@ -4,12 +4,7 @@ import ReactDOM from 'react-dom';
 
 const babarCharacters = ['Babar', 'Celeste', 'Flora', 'Pomme', 'Alexander', 'Isabel' ]
 const element = <ol>
-    <li> {babarCharacters[0]} </li> 
-    <li> {babarCharacters[1]} </li>
-    <li> {babarCharacters[2]} </li>
-    <li> {babarCharacters[3]} </li>
-    <li> {babarCharacters[4]} </li>
-    <li> {babarCharacters[5]} </li>
+    { babarCharacters.map((character) => <li> {character} </li>)}
 </ol>
 
 ReactDOM.render(element, document.getElementById('root'));
