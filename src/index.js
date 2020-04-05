7a5f2c0f111a05b94f474762ac628ac985e5c6b0
Emily Spencer <emilyspencer89@hotmail.co.uk>
2020-04-05 17:21:06 +0100

    Each array element is represented by a unique key prop

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom';
 
 const tasks = ['Rosie', 'and', 'Jim'];
 
-const element = React.createElement('ol', null, tasks.map((task) => React.createElement('li', null, task)))
+const element = React.createElement('ol', null, tasks.map((task, index) => React.createElement('li', { key: index }, task)))
 
 
 ReactDOM.render(element, document.getElementById('root'));
