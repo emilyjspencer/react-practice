2a4d381380570400909d7397a5bbd381240e0ea0
Emily Spencer <emilyspencer89@hotmail.co.uk>
2020-04-05 17:48:18 +0100

    Multiple JSX elements must be wrapped in a div

--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom';
 
 
 const babarCharacters = ['Babar', 'Celeste', 'Flora', 'Pomme', 'Alexander', 'Isabel' ]
-const element = <ol>
+
+
+const element = 
+    <div>
+      <h1>Babar the Elephant's family</h1>
+    <ol>
     { babarCharacters.map((character, index) => <li key={index}> {character} </li>)}
-</ol>
+   </ol>
+   </div>
 
 ReactDOM.render(element, document.getElementById('root'));
