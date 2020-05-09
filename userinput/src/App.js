7b5efad338fb84488b05c3bc1defa4ddaaca0c3d
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-05-09 18:48:00 +0100

    Creates userInput and userOutput components

---
+++ userinput/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import UserInput from './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput';
+
+function App() {
+  return (
+    <div className="App">
+      <UserInput />
+      <UserOutput />
+      <UserOutput />
+      <UserOutput />
+      </div>
+  );
+}
+
+export default App;
---
+++ userinput/src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const userOutput = () => {
+    return (
+        <div>
+            <h1>Hello</h1>
+            <h3>Bye</h3>
+        </div>
+    )
+}
+
+export default userOutput;
