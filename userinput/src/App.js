6b65c3f4ef020e1e5839f9b84de58a0c16ac9830
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-05-09 19:12:56 +0100

    Adds state to the App component and passes the usernames to the UserOutput components

--- userinput/src/App.js
+++ userinput/src/App.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import UserInput from './UserInput/UserInput';
 import UserOutput from './UserOutput/UserOutput';
 
-function App() {
-  return (
+class App extends Component {
+  state = {  
+    username1: "Friend1",
+    username2: "Friend2",
+    username3: "Friend3"
+  }
+
+  render() {
+    return (
     <div className="App">
       <UserInput />
-      <UserOutput username="Emily S" />
-      <UserOutput username="Julia S"/>
-      <UserOutput username="Claire G"/>
+      <UserOutput username={this.state.username1} />
+      <UserOutput username={this.state.username2} />
+      <UserOutput username={this.state.username3} />
       </div>
   );
+}
 }
 
 export default App;
