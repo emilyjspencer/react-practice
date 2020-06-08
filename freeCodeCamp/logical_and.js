0d2b0f859b214fae98db3148336dac274a0e4924
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-06-08 23:44:32 +0100

    Use of logical and for a more concise conditional

---
+++ freeCodeCamp/logical_and.js
@@ -0,0 +1,33 @@
+// Use && for a more concise conditional
+
+
+
+class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        display: true
+      }
+      this.toggleDisplay = this.toggleDisplay.bind(this);
+    }
+
+    toggleDisplay() {
+      this.setState(state => ({
+        display: !state.display
+      }));
+    }
+
+    render() {
+      
+      return (
+         <div>
+           <button onClick={this.toggleDisplay}>Toggle Display</button>
+           {this.state.display && <h1>Displayed!</h1>}
+           
+         </div>
+      );
+    }
+  };
+
+  // && can be used instead of if/else statements
+  
