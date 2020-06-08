8e5fc8faec4900881e390f83cbebfc3fa4e2c698
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-06-08 23:10:19 +0100

    Inline styles 2

---
+++ freeCodeCamp/inline_styles2.js
@@ -0,0 +1,13 @@
+// Inline styles 2
+
+const styles ={color: "purple", fontSize: 40, border: "2px solid purple" }
+
+class Colorful extends React.Component {
+  render() {
+   
+    return (
+      <div style={styles}>Style Me!</div>
+    );
+    
+  }
+};
