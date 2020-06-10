16ffef360f4606d4f6ed209199baa21a8b6e6473
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-06-10 15:33:44 +0100

    Define a Redux Action

---
+++ redux-freeCodeCamp/define_a_redux_action.js
@@ -0,0 +1,13 @@
+// Define a Redux Action 
+
+
+
+const action = {
+    type: 'LOGIN'
+}
+
+
+// With Redux, all state updates are triggered by dispatching actions
+// A Redux action is a JS object that contains info about an action event that has occurred
+// The Redux store receives the action objects and updates its state accordingly
+// Define an action by declaring an object with a type property 
